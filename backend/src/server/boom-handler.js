0f39723fdcf0ab0a2a5f5cb3877fc1c8661caeb2
yujiosaka <yujisobe@gmail.com>
2020-05-30 03:41:01 +0900

    Revert "chore: implement backend"

--- backend/src/server/boom-handler.js
+++
@@ -1,8 +0,0 @@
-function boomHandler(err, req, res, next) { // eslint-disable-line no-unused-vars
-  if (!err.output) return;
-  res.status(err.output.statusCode);
-  res.set(err.output.headers);
-  res.send(err.output.payload);
-}
-
-module.exports = boomHandler;
